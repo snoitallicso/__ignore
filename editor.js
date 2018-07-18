5a888de735fb9ab684d8e5d6f5fc45cf2b417106
snoitallicso <zerotype@tutanota.de>
2018-07-18 20:14:10 +0800

    Now admin side change states. Additionally in context content area is tabs with their own active states

--- editor.js
+++ editor.js
@@ -929,16 +929,108 @@ var elementsSlot = new Vue({
 	}
 })
 
+Vue.component('tab-customization', {
+  data: function () {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: 'General',
+          content: '<p>Dont wait for the storm to pass, dance in the rain kick up litter decide to want nothing to do with my owner today demand to be let outside at once, and expect owner to wait for me as i think about it cat cat moo moo lick ears lick paws so make meme, make cute face but lick the other cats. Kitty poochy chase imaginary bugs, but stand in front of the computer screen. Sweet beast cat dog hate mouse eat string barf pillow no baths hate everything stare at guinea pigs. My left donut is missing, as is my right loved it, hated it, loved it, hated it scoot butt on the rug cat not kitten around</p>'
+        },
+        {
+          id: 2,
+          title: 'Background',
+          content: '<p>Bushwick blue bottle scenester helvetica ugh, meh four loko. Put a bird on it lumbersexual franzen shabby chic, street art knausgaard trust fund shaman scenester live-edge mixtape taxidermy viral yuccie succulents. Keytar poke bicycle rights, crucifix street art neutra air plant PBR&B hoodie plaid venmo. Tilde swag art party fanny pack vinyl letterpress venmo jean shorts offal mumblecore. Vice blog gentrify mlkshk tattooed occupy snackwave, hoodie craft beer next level migas 8-bit chartreuse. Trust fund food truck drinking vinegar gochujang.</p>'
+        },
+        {
+          id: 3,
+          title: 'Design',
+          content: '<p>Icing dessert soufflé lollipop chocolate bar sweet tart cake chupa chups. Soufflé marzipan jelly beans croissant toffee marzipan cupcake icing fruitcake. Muffin cake pudding soufflé wafer jelly bear claw sesame snaps marshmallow. Marzipan soufflé croissant lemon drops gingerbread sugar plum lemon drops apple pie gummies. Sweet roll donut oat cake toffee cake. Liquorice candy macaroon toffee cookie marzipan.</p>'
+        }
+      ],
+      selectedPost: null
+    }
+  },
+  template: '\
+    <div class="sidebar_content">\
+      <div class="sidebar_title"><h1>Sidebar Title</h1></div>\
+      <div class="sidebar_tabs"><ul>\
+        <li\
+          v-for="post in posts"\
+          v-bind:key="post.id"\
+          v-bind:class="[\'side_tab\', { \'active\': post === selectedPost }]"\
+          v-on:click="selectedPost = post"\
+        >\
+          {{ post.title }}\
+        </li>\
+      </ul></div>\
+      <div class="content_area">\
+        <div \
+          v-if="selectedPost"\
+          class="dynamic-component-demo-post"\
+        >\
+          <h3>{{ selectedPost.title }}</h3>\
+          <div v-html="selectedPost.content"></div>\
+        </div>\
+        <strong v-else>\
+          Click on a blog title to the left to view it.\
+        </strong>\
+      </div>\
+    </div>\
+  ',
+  // template: '\
+  //   <div class="">\
+  //     <div class="sidebar_title"><h1>Sidebar Title</h1></div>\
+  //     <div class="sidebar_tabs"><ul>\
+  //       <li\
+  //         v-for="post in posts"\
+  //         v-bind:key="post.id"\
+  //         v-bind:class="[\'side_tab\', { \'active\': post === selectedPost }]"\
+  //         v-on:click="selectedPost = post"\
+  //       >\
+  //         {{ post.title }}\
+  //       </li>\
+  //     </ul></div>\
+  //     <div class="content_area">\
+  //       <div \
+  //         v-if="selectedPost"\
+  //         class="dynamic-component-demo-post"\
+  //       >\
+  //         <h3>{{ selectedPost.title }}</h3>\
+  //         <div v-html="selectedPost.content"></div>\
+  //       </div>\
+  //       <strong v-else>\
+  //         Click on a blog title to the left to view it.\
+  //       </strong>\
+  //     </div>\
+  //   </div>\
+  // ',
+  methods: {
+    alert: function(message){
+      console.log(message)
+    }
+  }
+})
+
+Vue.component('tab-pages', {
+  template: '<div>Pages component</div>'
+})
+Vue.component('tab-options', {
+  template: '<div>Options component</div>'
+})
+
 new Vue({
 	el: "#contextside",
 	data: {
-    currentTab: 'Posts',
-    tabs: ['Posts', 'Archive']
-  },
-  computed: {
-    // currentTabComponent: function () {
-    //   console.log('tab-' + this.currentTab.toLowerCase())
-    //   return 'tab-' + this.currentTab.toLowerCase()
-    // }
-  }
+		currentTab: 'Customization',
+		tabs: ['Customization', 'Pages', 'Options']
+	},
+	computed: {
+		currentTabComponent: function () {
+		  console.log('tab-' + this.currentTab.toLowerCase())
+		  return 'tab-' + this.currentTab.toLowerCase()
+		}
+	}
 })
+
