8788e5aa35e12a7130f527545b1e7ca23224b19a
snoitallicso <zerotype@tutanota.de>
2018-08-02 04:50:12 +0800

    Add: common state object between context in the sidebar and element model, relocate template to html

--- editor.js
+++ editor.js
@@ -8,6 +8,7 @@ templ0 = elpretempl1.innerHTML
 
 templ1 = document.getElementsByClassName("templ")[1].innerHTML;
 templ2 = document.getElementsByClassName("templ")[0].innerHTML;
+templ3 = document.getElementsByClassName("templ_three")[0].innerHTML;
 
 Array.prototype.insert = function(index, item){
 	this.splice(index, 0, item)
@@ -554,86 +555,41 @@ var elementsSlot = new Vue({
 	}
 })
 
+//centralized state for context of selected element
+var contextTabs = [
+  {
+    id: 1,
+    title: 'General',
+    content: '<p>Dont wait for the storm to pass, dance in the rain kick up litter decide to want nothing to do with my owner today demand to be let outside at once, and expect owner to wait for me as i think about it cat cat moo moo lick ears lick paws so make meme, make cute face but lick the other cats. Kitty poochy chase imaginary bugs, but stand in front of the computer screen. Sweet beast cat dog hate mouse eat string barf pillow no baths hate everything stare at guinea pigs. My left donut is missing, as is my right loved it, hated it, loved it, hated it scoot butt on the rug cat not kitten around</p>'
+  },
+  {
+    id: 2,
+    title: 'Background',
+    content: '<p>Bushwick blue bottle scenester helvetica ugh, meh four loko. Put a bird on it lumbersexual franzen shabby chic, street art knausgaard trust fund shaman scenester live-edge mixtape taxidermy viral yuccie succulents. Keytar poke bicycle rights, crucifix street art neutra air plant PBR&B hoodie plaid venmo. Tilde swag art party fanny pack vinyl letterpress venmo jean shorts offal mumblecore. Vice blog gentrify mlkshk tattooed occupy snackwave, hoodie craft beer next level migas 8-bit chartreuse. Trust fund food truck drinking vinegar gochujang.</p>'
+  },
+  {
+    id: 3,
+    title: 'Design',
+    content: '<p>Icing dessert soufflé lollipop chocolate bar sweet tart cake chupa chups. Soufflé marzipan jelly beans croissant toffee marzipan cupcake icing fruitcake. Muffin cake pudding soufflé wafer jelly bear claw sesame snaps marshmallow. Marzipan soufflé croissant lemon drops gingerbread sugar plum lemon drops apple pie gummies. Sweet roll donut oat cake toffee cake. Liquorice candy macaroon toffee cookie marzipan.</p>'
+  }
+];
+
 Vue.component('tab-customization', {
   data: function () {
     return {
-      posts: [
-        {
-          id: 1,
-          title: 'General',
-          content: '<p>Dont wait for the storm to pass, dance in the rain kick up litter decide to want nothing to do with my owner today demand to be let outside at once, and expect owner to wait for me as i think about it cat cat moo moo lick ears lick paws so make meme, make cute face but lick the other cats. Kitty poochy chase imaginary bugs, but stand in front of the computer screen. Sweet beast cat dog hate mouse eat string barf pillow no baths hate everything stare at guinea pigs. My left donut is missing, as is my right loved it, hated it, loved it, hated it scoot butt on the rug cat not kitten around</p>'
-        },
-        {
-          id: 2,
-          title: 'Background',
-          content: '<p>Bushwick blue bottle scenester helvetica ugh, meh four loko. Put a bird on it lumbersexual franzen shabby chic, street art knausgaard trust fund shaman scenester live-edge mixtape taxidermy viral yuccie succulents. Keytar poke bicycle rights, crucifix street art neutra air plant PBR&B hoodie plaid venmo. Tilde swag art party fanny pack vinyl letterpress venmo jean shorts offal mumblecore. Vice blog gentrify mlkshk tattooed occupy snackwave, hoodie craft beer next level migas 8-bit chartreuse. Trust fund food truck drinking vinegar gochujang.</p>'
-        },
-        {
-          id: 3,
-          title: 'Design',
-          content: '<p>Icing dessert soufflé lollipop chocolate bar sweet tart cake chupa chups. Soufflé marzipan jelly beans croissant toffee marzipan cupcake icing fruitcake. Muffin cake pudding soufflé wafer jelly bear claw sesame snaps marshmallow. Marzipan soufflé croissant lemon drops gingerbread sugar plum lemon drops apple pie gummies. Sweet roll donut oat cake toffee cake. Liquorice candy macaroon toffee cookie marzipan.</p>'
-        }
-      ],
+      posts: contextTabs,
       selectedPost: null
     }
   },
-  template: '\
-    <div class="sidebar_content">\
-      <div class="sidebar_title"><h1>Sidebar Title</h1></div>\
-      <div class="sidebar_tabs"><ul>\
-        <li\
-          v-for="post in posts"\
-          v-bind:key="post.id"\
-          v-bind:class="[\'side_tab\', { \'active\': post === selectedPost }]"\
-          v-on:click="selectedPost = post"\
-        >\
-          {{ post.title }}\
-        </li>\
-      </ul></div>\
-      <div class="content_area">\
-        <div \
-          v-if="selectedPost"\
-          class="dynamic-component-demo-post"\
-        >\
-          <h3>{{ selectedPost.title }}</h3>\
-          <div v-html="selectedPost.content"></div>\
-        </div>\
-        <strong v-else>\
-          Click on a blog title to the left to view it.\
-        </strong>\
-      </div>\
-    </div>\
-  ',
-  // template: '\
-  //   <div class="">\
-  //     <div class="sidebar_title"><h1>Sidebar Title</h1></div>\
-  //     <div class="sidebar_tabs"><ul>\
-  //       <li\
-  //         v-for="post in posts"\
-  //         v-bind:key="post.id"\
-  //         v-bind:class="[\'side_tab\', { \'active\': post === selectedPost }]"\
-  //         v-on:click="selectedPost = post"\
-  //       >\
-  //         {{ post.title }}\
-  //       </li>\
-  //     </ul></div>\
-  //     <div class="content_area">\
-  //       <div \
-  //         v-if="selectedPost"\
-  //         class="dynamic-component-demo-post"\
-  //       >\
-  //         <h3>{{ selectedPost.title }}</h3>\
-  //         <div v-html="selectedPost.content"></div>\
-  //       </div>\
-  //       <strong v-else>\
-  //         Click on a blog title to the left to view it.\
-  //       </strong>\
-  //     </div>\
-  //   </div>\
-  // ',
+  template: templ3,
   methods: {
     alert: function(message){
-      console.log(message)
+      alert(message)
+    },
+    setData: function(dataobject){
+    	console.log(dataobject)
+    	x = this
+    	console.log(this)
     }
   }
 })
@@ -659,3 +615,5 @@ new Vue({
 	}
 })
 
+tabcust = Vue.component("tab-customization");
+tabcust.options.methods.setData("yeah!")
